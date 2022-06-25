91314ee776cd080c05ab31e09f2919069fab1b7e
Joel Rojas <joel@Joels-MacBook-Pro.local>
2022-06-25 18:42:48 +0200

    operatorsDone

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function suma(){
-    document.getElementById("ss").value = "puto";
+    let num1 = parseInt(document.getElementById("n1").value);
+    let num2 = parseInt(document.getElementById("n2").value);
+    let resu = num1+num2;
+    document.getElementById("re").innerHTML = resu;
+
+    //document.getElementById("s1").value = "suma";
 }
 function resta(){
-    document.getElementById("ss").value = "puto";
+    let num1 = parseInt(document.getElementById("n1").value);
+    let num2 = parseInt(document.getElementById("n2").value);
+    let resu = num1-num2;
+    document.getElementById("re").innerHTML = resu;
+    // document.getElementById("s2").value = "rest";
 }
 function multi(){
-    document.getElementById("ss").value = "puto";
+    let num1 = parseInt(document.getElementById("n1").value);
+    let num2 = parseInt(document.getElementById("n2").value);
+    let resu = num1*num2;
+    document.getElementById("re").innerHTML = resu;
+    // document.getElementById("s3").value = "mult";
 }
 function divi(){
-    document.getElementById("ss").value = "puto";
+    let num1 = parseInt(document.getElementById("n1").value);
+    let num2 = parseInt(document.getElementById("n2").value);
+    let resu = num1/num2;
+    document.getElementById("re").innerHTML = resu;
+    // document.getElementById("s4").value = "div";
 }
 function igual(){
-    document.getElementById("ss").value = "puto";
+    document.getElementById("s5").value = "igual";
 }
