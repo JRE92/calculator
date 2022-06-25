537ba7cecabf3db8f74ac585e21f1ba8ef8ad278
Joel Rojas <joel@Joels-MacBook-Pro.local>
2022-06-25 15:05:01 +0200

    ft

---
+++ script.js
@@ -0,0 +1,15 @@
+function suma(){
+    document.getElementById("ss").value = "puto";
+}
+function resta(){
+    document.getElementById("ss").value = "puto";
+}
+function multi(){
+    document.getElementById("ss").value = "puto";
+}
+function divi(){
+    document.getElementById("ss").value = "puto";
+}
+function igual(){
+    document.getElementById("ss").value = "puto";
+}
